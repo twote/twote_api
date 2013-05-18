616e6ac6d4b1308a81b399bfe251341925e8210b
sreuter <sascha@tape.io>
2013-05-19 01:29:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var Twit = require('twit'),
+    config = require('./config');
+
+// Setup CouchDB connection
+
+var db = new(require('cradle').Connection)(config.db.uri).database(config.db.name);
+
+var T = new Twit({
+  consumer_key: config.twitter.consumerKey,
+  consumer_secret: config.twitter.consumerSecret,
+  access_token: config.twitter.accessToken,
+  access_token_secret: config.twitter.tokenSecret
+});
+
+var stream = T.stream('statuses/filter', {
+  track: '#' + config.hashId
+});
+
+stream.on('tweet', function(tweet) {
+  db.save(tweet.id_str, tweet);
+});
