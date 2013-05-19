f38e5e3a357d5f32bc5ac67aa159aa4e4e722178
Sascha Reuter <s.reuter@geek-it.de>
2013-05-19 19:24:39 +0000

    add api routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Twit = require('twit'),
+    cors = require('cors'),
     config = require('./config');
 
 // Make process "fault tolerant" ;-)
@@ -46,10 +47,126 @@ function nice_votes(db_result) {
   return votes;
 }
 
+function nice_votes_api(db_result) {
+  var niceVotes     = nice_votes(db_result),
+      niceVotesApi = [],
+      i,
+      k,
+      current,
+      improved,
+      overallVotes = 0;
+
+  for (i in niceVotes) {
+    if (niceVotes.hasOwnProperty(i)) {
+      current = niceVotes[i];
+
+      // calculate overall votes
+      overallVotes = 0;
+      for (k in current) {
+        if (current.hasOwnProperty(k)) {
+          overallVotes += current[k];
+        }
+      }
+
+      improved = {
+        twote: i,
+        overall_votes: overallVotes
+      };
+
+      niceVotesApi.push(improved);
+    }
+  }
+
+  return niceVotesApi;
+}
+
+function nice_votes_api_single(db_result, twoteId) {
+  var niceVotes     = nice_votes(db_result),
+      niceVotesApi = [],
+      i,
+      k,
+      current,
+      improved,
+      overallVotes = 0;
+
+  for (i in niceVotes) {
+    if (niceVotes.hasOwnProperty(i)) {
+      current = niceVotes[i];
+
+      // calculate overall votes
+      overallVotes = 0;
+      for (k in current) {
+        if (current.hasOwnProperty(k)) {
+          overallVotes += current[k];
+        }
+      }
+
+      improved = {
+        twote: i,
+        overall_votes: overallVotes,
+        votes: current
+      };
+
+      niceVotesApi.push(improved);
+    }
+  }
+
+  if (niceVotesApi.length === 0) {
+    niceVotesApi.push({
+      twote: twoteId,
+      overall_votes: 0,
+      votes: {}
+    });
+  }
+
+  return niceVotesApi[0];
+}
+
 // Setup express
 
 var express = require('express');
 var app = express();
+
+app.use(cors());
+
+app.get('/twote', function (req, res) {
+  var result;
+
+  var query = {
+    group: true,
+    reduce: true
+  };
+
+  db.view('votes/sumVotesByPoll', query, function (err, db_result) {
+    if (err) {
+      console.log(err);
+      return res.send(500);
+    }
+    res.json({
+      result: nice_votes_api(db_result)
+    });
+  });
+});
+
+app.get('/twote/:id', function (req, res) {
+  var result;
+
+  var query = {
+    group: true,
+    reduce: true,
+    key: req.param('id')
+  };
+
+  db.view('votes/sumVotesByPoll', query, function (err, db_result) {
+    if (err) {
+      console.log(err);
+      return res.send(500);
+    }
+    res.json({
+      result: nice_votes_api_single(db_result, req.param('id'))
+    });
+  });
+});
 
 app.get('/:poll?', function(req, res){
   var query = {
