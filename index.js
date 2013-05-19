28c16c290f6879ef063f2a7d8c3028cf8d60ad99
Sascha Reuter <s.reuter@geek-it.de>
2013-05-19 17:17:39 +0000

    Use new view logic in index.js

--- index.js
+++ index.js
@@ -23,5 +23,36 @@ var stream = T.stream('statuses/filter', {
 });
 
 stream.on('tweet', function(tweet) {
+ console.log(tweet);
   db.save(tweet.id_str, tweet);
 });
+
+// Setup express
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  db.view('votes/sumVotesByPoll', {group: true, reduce: true}, function(err, db_result) {
+    if(err) {
+      console.log(err)
+      return res.send(500);
+    }
+    var votes = {}, voteHash;
+    for(var i in db_result) {
+      hash = db_result[i].key;
+       // Create vote if not existing yet
+      if(! votes[hash]) {
+        votes[hash] = {};
+      }
+      for (var option in db_result[i].value.votes) {
+        counter = Object.keys(db_result[i].value.votes[option]).length;
+        // Set choice counter
+        votes[hash][option] = counter;
+      }
+    }
+    res.send(votes);
+  });
+});
+
+app.listen(8000);
